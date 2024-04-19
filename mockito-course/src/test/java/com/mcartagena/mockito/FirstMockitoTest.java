b65f09364e8e48723ec7b09a0b7cd05a140e53e4
Marcelo Cartagena <marcelocartagenaorellana@gmail.com>
2024-04-19 11:00:24 -0400

    fea: adding mockito-course project

---
+++ mockito-course/src/test/java/com/mcartagena/mockito/FirstMockitoTest.java
@@ -0,0 +1,12 @@
+package com.mcartagena.mockito;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FirstMockitoTest {
+    @Test
+    public void test(){
+        assertTrue(true);
+    }
+}
